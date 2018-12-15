065c770a2aef9be68288c9528a2251e4d918b1c6
Bharani <bharanidharan.nb@gmail.com>
2018-12-15 20:44:13 +0530

    Implement sequencer functions

--- src/sequencers/partialSumSequence.js
+++ src/sequencers/partialSumSequence.js
@@ -1,5 +1,11 @@
 function* partialSumSequence(...numbers) {
-  yield numbers;
+  let currentSum = 0;
+  let index = 0;
+
+  for (index = 0; index < numbers.length; index += 1) {
+    currentSum += numbers[index];
+    yield currentSum;
+  }
 }
 
 export { partialSumSequence };
